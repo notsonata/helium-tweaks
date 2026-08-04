2155773bb7f95e94e378d0c8784c8619ea6d1c75
Angelo David <90495108+notsonata@users.noreply.github.com>
2026-08-05 02:50:08 +0800

    Load bookmark rename handler in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,6 @@
 /* Classic MV3 service-worker entry point. */
-importScripts("background.js", "bookmark-dnd-background.js");
+importScripts(
+  "background.js",
+  "bookmark-dnd-background.js",
+  "bookmark-rename-background.js"
+);
